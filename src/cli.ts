7f2041338038e7d5da45558d4ddb02fb64792aad
Masaya Takeda <844196@gmail.com>
2019-06-21 17:52:57 +0900

    implemented empty command

---
+++ src/cli.ts
@@ -0,0 +1,14 @@
+import cac from 'cac';
+import me from '../package.json';
+import process from 'process';
+
+const cli = cac(me.name)
+  .version(me.version)
+  .help();
+
+cli.on('command:*', () => {
+  console.error(`Unknown sub command: ${cli.args[0]}`);
+  process.exit(1);
+});
+
+export { cli };
